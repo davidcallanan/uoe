0a6fb3cc9e56108ddccac34a894370f2636c77de
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2022-12-22 15:51:50 +0000

    update

---
+++ iter_arr_reverse.js
@@ -0,0 +1,17 @@
+export const iter_arr_reverse = (arr) => {
+	return {
+		[Symbol.iterator]: () => {
+			let idx = arr.length;
+
+			return {
+				next() {
+					idx--;
+					return {
+						done : idx < 0,
+						value : arr[idx]
+					}
+				}
+			};
+		},
+	};
+};
