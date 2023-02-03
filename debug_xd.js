9f045865576dd6c0bc8ff946aa5014a7958dfe2b
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2023-02-03 01:24:32 +0000

    update

---
+++ debug_xd.js
@@ -0,0 +1,35 @@
+const extra_data = new Map();
+
+/**
+ * This function allows you to attach extra data to any object for temporary debugging purposes.
+ * It essentially allows you to carry some data around that is tied to / associated with an object.
+ * It is especially useful for objects that make it difficult to hackishly modify custom properties in moments of debugging.
+ * 
+ * @example
+ * 
+ * const john = create_person();
+ * debug_xd(john).name = "john";
+ * 
+ * const jane = create_person();
+ * debug_xd(jane).name = "jane";
+ * 
+ * // later on, we detect an error originating in some person object and we'd like to log its name
+ * const person = get_arbitrary_person();
+ * 
+ * try {
+ *   person.do_something();
+ * } catch(e) {
+ *   console.log(debug_xd(person).name, "caused the error");
+ * }}
+ */
+export const debug_xd = (obj) => {
+	if (extra_data.has(obj)) {
+		return extra_data.get(obj);
+	}
+
+	const xd = {};
+	extra_data.set(obj, xd);
+	return xd;	
+};
+
+export const debugXd = debug_xd;
