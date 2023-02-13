5b9cb484c4ef080652030f6456b2996211b30208
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2023-02-13 14:26:18 +0000

    add call_as_async

---
+++ call_as_async.js
@@ -0,0 +1,10 @@
+/**
+ * Calls a function that may or may not be async with guaranteed async behaviour.
+ * This is useful when invoking an externally-provided function that may not be async.
+ * It ensures that any errors originating in the provided function are incorporated into the promise as opposed to the current execution flow.
+ */
+export const call_as_async = async (async_func) => {
+	return await async_func();
+};
+
+export const callAsAsync = call_as_async;
