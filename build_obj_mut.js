104ef607fc50fc0b3e1f90ca2b06002db450b8f5
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2023-02-13 14:05:47 +0000

    add build_obj_mut

---
+++ build_obj_mut.js
@@ -0,0 +1,15 @@
+/**
+ * Similar to `build_obj` except that each steps mutates the initial object.
+ * The first argument is taken as-is, and the remaining arguments are treated as steps.
+ * This means fancy objects like functions can be passed in as the first argument.
+ */
+export const build_obj_mut = (initial_obj, ...steps) => {
+	for (let step of steps) {
+		let next = typeof step === "function" ? step(obj) : step;
+		Object.assign(initial_obj, next);
+	}
+
+	return initial_obj;
+};
+
+export const buildObjMut = build_obj_mut;
