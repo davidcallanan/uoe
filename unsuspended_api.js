09316e30e772658d5ee14a5a0a0d1e6956141589
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2023-02-25 02:18:59 +0000

    update unsuspended_api

--- unsuspended_api.js
+++ unsuspended_api.js
@@ -3,9 +3,20 @@ import { user_error, internal_error } from "./error.js";
 /**
  * Wraps an api or promise to an api such that the api is immediately available. Properties and methods, including sub-properties of the api and sub-properties of the return-values of methods, are proxied to be immediately accessible before the api has neccesserily been resolved, and before parent properties and return-values of parent methods have been resolved. However, this means that the properties and methods are forced to be async.
  * 
+ * Upon access of a property or invocation of the wrapped api, it will first wait for the underlying api to be resolved before proceeding.
+ * 
+ * If errors are reported by the passed-in promise, these errors will instead be delivered as internal errors inside any promises obtained from the wrapped api.
+ * 
+ * Warning: The `then`, `catch` and `finally` properties must not be defined on the underlying api become as this makes the underlying api promise-like, while also conflicting with our implementation of the promise interface at every level.
+ * Doing this will cause bizarre behaviour which may be difficult to track down.
+ * 
  * @example
  * 
  * const api = unsuspended_api(api_promise);
+ * 
+ * // The following two lines of code are equivalent
+ * await api;
+ * await api_promise;
  * 
  * // The following two lines of code are equivalent
  * await api();
@@ -30,8 +41,6 @@ import { user_error, internal_error } from "./error.js";
  * // As you can see, unsuspending an api removes a lot of await statements.
  * // This offers a high degree of flexibility when working with asynchronous code.
  * 
- * // TODO: update implementation to match new desired functionality as documented here.
- * 
  * @example
  * 
  * const create_opengl_api = async () => {
@@ -49,7 +58,9 @@ import { user_error, internal_error } from "./error.js";
  * console.log("triangle drawn");
  */
 
-export const unsuspended_api = (api_promise) => {
+export const unsuspended_api = (api_like) => {
+	const api_promise = Promise.resolve(api_like);
+
 	const get_api = async (api_promise) => {
 		try {
 			return await api_promise;
@@ -58,49 +69,19 @@ export const unsuspended_api = (api_promise) => {
 		}
 	};
 
-	(async () => {
-		try {
-			(await api_promise).then !== undefined &&
-				console.warn("Resolved api object must not be thenable due to a JavaScript promises anomoly. The then property has been forcefully unset.");
-			;
-		} catch (e) {
-			console.error(e);
-		}
-	})();
-
 	return new Proxy(async (...args) => {
 		const api = await get_api(api_promise);
 		return await api(...args);
 	}, {
-		get: (_target, prop) => {
-			if (prop === "then") {
-				// We need to make sure the final object is not "thenable", that is, not "promise-like".
-				// This is due to an anomoly with the promises implementation where thenable objects are treated differently than regular objects during resolution.
-				// This would prevent the final object from ever being resolved for a promise.
-				// Promises are so widely used that it would not take long for this to cause major problems.
-				// As such, we are forced to forcefully unset the "then" property on the final object.
-				// It only took a full day's work to track down a bug caused by this anomoly.
-
-				return undefined;
+		get: (target, prop) => {
+			if (["then", "catch", "finally"].includes(prop)) {
+				return api_promise[prop];
 			}
 
-			if (prop.startsWith("_")) {
-				return undefined;
-			}
-
-			return async (...args) => {
+			return unsuspended_api((async () => {
 				const api = await get_api(api_promise);
-
-				if (typeof api[prop] !== "function") {
-					if (api[prop] !== undefined) {
-						console.error(api[prop]);
-					}
-
-					throw user_error(`API does not have method ${prop}`);
-				}
-
-				return await api[prop](...args);
-			};
+				return api[prop];
+			})());
 		},
 	});
 };
