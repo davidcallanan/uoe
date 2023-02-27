10d4ce4b203f6b1bec885c73ccbb33f63f2a3cbe
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2023-02-27 17:36:33 +0000

    fix callable_bound

--- callable_bound.js
+++ callable_bound.js
@@ -1,4 +1,5 @@
 import { lazy_bind } from "./lazy_bind.js";
+import { callable } from "./callable.js";
 
 export const callable_bound = (obj, call) => {
 	const result = lazy_bind(callable(obj, call), () => result);
