e21e03416e4379ba4e936b40214b53889f34d7fc
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2023-02-28 14:41:16 +0000

    fix as_async

--- as_async.js
+++ as_async.js
@@ -1,7 +1,7 @@
 export const as_async = (possibly_async_func) => {
 	// Not using arrow function to ensure `this` value is maintained.
 	return async function (...args) {
-		return await possibly_async_func(...args);
+		return await Reflect.apply(possibly_async_func, this, args);
 	};
 };
 
