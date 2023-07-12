e568b5459de9791772bb70a92034e7854b5b0be0
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2023-07-12 03:03:32 +0100

    add simple_search

---
+++ simple_search.js
@@ -0,0 +1,40 @@
+const count_instances = (string, substring) => {
+	return string.split(substring).length - 1;
+};
+
+const count_once = (string, substring) => {
+	return string.includes(substring) ? 1 : 0;
+};
+
+export const simple_search = (array, query, options) => {
+	options ??= {};
+	options.portion_size ??= 4;
+	options.keys ??= [{
+		key: "key",
+		weight: 1,
+	}];
+	options.max_results ??= 5;
+	
+	const { portion_size, keys, max_results } = options;
+	
+	array = [...array];
+
+	let portions = [];
+
+	for (let i = 0; i < query.length - (portion_size - 1); i++) {
+		portions.push(query.substring(i, i + portion_size));
+	}
+
+	for (let entry of array) {
+		let score = 0;
+		for (let portion of portions) {
+			for (let key of keys) {
+				score += count_once(entry[key.key], portion) * (key.weight ?? 1);
+			}
+		}
+		
+		entry.score = score;
+	}
+
+	return array.filter(entry => entry.score !== 0).map(entry => ({ ...entry })).sort((a, b) => b.score - a.score).slice(0, max_results);
+};
