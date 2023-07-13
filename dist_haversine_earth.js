c3b3363087306b7c6904ceca25dccaf1a07618ef
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2023-07-13 04:03:30 +0100

    add dist_haversine

---
+++ dist_haversine_earth.js
@@ -0,0 +1,5 @@
+const EARTH_RADIUS_KM = 6371;
+
+export const dist_haversine_earth = (location_a, location_b) => dist_haversine(location_a, location_b, EARTH_RADIUS_KM);
+
+export const distHaversineEarth = dist_haversine_earth;
