f9eccf7881be491037a1954572dabaabd7b5ca6b
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2024-03-16 18:08:35 +0000

    add get_enum

---
+++ get_enum.js
@@ -0,0 +1,20 @@
+import { is_enum } from "./is_enum";
+
+/**
+ * Checks if a uoe-map is an enum and subsequently returns it.
+ */
+export const get_enum = async (object) => {
+	if (is_map(object)) {
+		const map = object;
+		const result = await map();
+		return await get_enum(result);
+	}
+
+	if (is_enum(object)) {
+		return object;
+	}
+
+	return undefined;
+};
+
+export const getEnum = get_enum;
