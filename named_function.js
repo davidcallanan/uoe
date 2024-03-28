191060c1093f0620e9cf0dfad94e7535c708b4e4
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2024-03-28 19:51:28 +0000

    fix named_function

--- named_function.js
+++ named_function.js
@@ -2,7 +2,7 @@
  * Creates a function that is dynamically named at run-time.
  */
 export const named_function = (name, func) => {
-	return { [name]: function(...args) { return func.apply(this, args) } }[name];
+	return { [name]: function(...args) { return Reflect.apply(func, this, args) } }[name];
 };
 
 export const namedFunction = named_function;
