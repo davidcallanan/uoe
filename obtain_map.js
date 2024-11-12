326dc4c99effb1d738a42c88c15681c13a470eb0
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2024-11-12 22:18:46 +0000

    add obtain_map

---
+++ obtain_map.js
@@ -0,0 +1,23 @@
+import { map } from "./map.js";
+import { is_map } from "./is_map.js";
+
+/**
+ * Creates a map immediately from a promise-like to a map.
+ * 
+ * Immediately returns a map equal to that eventually obtained by the given promise.
+ * 
+ * Room for alteration: Will eventually use this function to add type information to an existing map.
+ */
+export const obtain_map = (promise_like) => {
+	if (is_map(promise_like)) {
+		const the_map = promise_like;
+		return the_map;
+	}
+
+	const promise = Promise.resolve(promise_like);
+
+	return map(async (input) => {
+		const the_map = await promise;
+		return await the_map(input);
+	});
+};
--- obtain_map.test.js
+++ obtain_map.test.js
@@ -1,27 +1,40 @@
 import { test } from "./test.js";
 import { map } from "./map.js";
 import { unsuspended_promise } from "./unsuspended_promise.js";
+import { obtain_map } from "./obtain_map.js";
+
+const mix = (map) => {
+	const m1 = map;
+	const p1 = unsuspended_promise(m1);
+	const m2 = obtain_map(p1);
+	const p2 = unsuspended_promise(m2);
+	const m3 = obtain_map(p2);
+	const p3 = unsuspended_promise(m3);
+	const m4 = obtain_map(p3);
+
+	return m4;
+};
 
 await test("basic test", async () => {
-	const m = map((input) => {
+	const m = mix(map((input) => {
 		if (input === undefined) {
 			return "hello";
 		}
 
 		return "world";
-	});
+	}));
 
 	return await m() === "hello" && await m.foo() === "world";
 });
 
 await test("unsuspension", async () => {
-	const m = map((input) => {
+	const m = mix(map((input) => {
 		if (input === undefined) {
 			return "hello";
 		}
 
 		return "world";
-	});
+	}));
 
 	const m2 = unsuspended_promise(m);
 
@@ -29,39 +42,39 @@ await test("unsuspension", async () => {
 });
 
 await test("complex test", async () => {
-	const m = map((input) => {
+	const m = mix(map((input) => {
 		if (input === undefined) {
 			return undefined;
 		}
 
-		return map((input) => {
+		return mix(map((input) => {
 			if (input === undefined) {
 				return "hello";
 			}
-		});
-	});
+		}));
+	}));
 
 	return await m.test() === "hello";
 });
 
 await test("complex test 2", async () => {
-	const m = map((input) => {
+	const m = mix(map((input) => {
 		if (input === undefined) {
 			return undefined;
 		}
 
-		return map((input) => {
+		return mix(map((input) => {
 			if (input === undefined) {
 				return undefined;
 			}
 
-			return map((input) => {
+			return mix(map((input) => {
 				if (input === undefined) {
 					return "hello";
 				}
-			});
-		})
-	});
+			}));
+		}));
+	}));
 
 	return await m.test.test() === "hello";
 });
