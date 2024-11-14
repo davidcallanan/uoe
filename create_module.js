dbdb8ec7eac3faa1c03720647806365615169fd0
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2024-11-14 16:06:38 +0000

    update create_module

--- create_module.js
+++ create_module.js
@@ -1,4 +1,7 @@
 import { as_async } from "./as_async.js";
+import { empty_map } from "./empty_map.js";
+import { is_map } from "./is_map.js";
+import { pattern_map } from "./pattern_map.js";
 
 /**
  * Creates a module which is an intermediate organizational tool for building a map.
@@ -11,39 +14,72 @@ import { as_async } from "./as_async.js";
  * 4. Segregating implementation into chunks, for the purpose of allowing the developer to split the implementation across files.
  * 5. Obtaining a final map factory for export.
  * 
- * Takes in an async initialization function, which takes in the environment (a map) provided by the developer when they invoke the map factory, and returns an updated environment which is passed to your implementation, allowing for any needed initialization logic.
+ * Takes in an optional async initialization function, which takes in the environment (a map) provided by the developer when they invoke the map factory, and returns an updated environment which is passed to your implementation, allowing for any needed initialization logic.
+ * 
+ * @example
+ * 
+ * // mod.ts
+ * 
+ * import function_a from "./function_a.js";
+ * import function_b from "./function_b.js";
+ * 
+ * const module = create_module(async (env) => {
+ *   // initialization logic here
+ * });
+ * 
+ * module.impl(function_a);
+ * module.impl(function_b);
+ * 
+ * export default module.create_map_factory();
+ * 
+ * // function_a.js
+ * 
+ * export default (env) => [
+ *   ["function_a", "banana"],
+ * ];
+ * 
+ * // function_b.js
+ * 
+ * export default (env) => [
+ *   ["function_b", "octopus"],
+ * ];
+ * 
+ * // main.ts
+ * 
+ * import create_map from "./mod.js";
+ * 
+ * const map = create_map();
+ * 
+ * await map.function_a(); // "banana"
+ * await map.function_b(); // "octopus"
  */
 export const create_module = (init) => {
-	const init_async = as_async(init);
+	const init_async = as_async(init ?? (() => {}));
+	const impl_chunks = [];
 
 	return {
+		impl: (callback) => {
+			impl_chunks.push(callback);
+		},
 		create_map_factory: () => {
 			return (env) => obtain_map((async () => {
-				const new_env_candidate = await init_async(env);
-				const new_env = new_env_candidate ?? env;
+				env ??= empty_map;
 
-				return map((input) => {
-					throw "Not implemented.";
-				});
+				if (!is_map(env)) {
+					throw TypeError("`env` must be a map.");
+				}
+
+				env = await init_async(env) ?? env;
+
+				if (!is_map(env)) {
+					throw TypeError("`env` must be a map.");
+				}
+
+				const processed = [...impl_chunks.map(impl_chunk => impl_chunk(env))];
+				return pattern_map(processed);
 			})());
 		},
 	};
 };
 
-// Example usage:
-
-// const module = create_module(async (env) => {
-// 	// iniitalization logic here.
-// });
-
-// module.implement((ctx, env) => {
-// 	ctx.define("foo", () => {
-
-// 	});
-// });
-
-// export default (module, env) => {
-// 	module.public("foo", )
-// };
-
-// export default module.create_map_factory();
+export const createModule = create_module;
