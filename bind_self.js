af0dfe125b8e26d41c3311c04fd0d5a38598e2b5
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-01-03 19:18:41 +0000

    doc

--- bind_self.js
+++ bind_self.js
@@ -8,6 +8,7 @@ import { lazy_bind } from "./lazy_bind.js";
  * This is useful for creating callable objects where `this` refers to the callable object itself.
  */
 export const bind_self = (func) => {
+	// Note: The usage of `var` is intentional.
 	var result = lazy_bind(func, () => result);
 	return result;
 };
