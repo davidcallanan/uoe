db00300485bb8cfa2b5f46d92ad2a14537cd8468
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-01-03 19:45:11 +0000

    test dist_haversine

--- dist_haversine.js
+++ dist_haversine.js
@@ -1,3 +1,5 @@
+const TO_RADIANS = Math.PI / 180;
+
 /**
  * @stability 3 - stable
  * 
@@ -6,19 +8,19 @@
  * This function was adapted from https://stackoverflow.com/q/18883601/1541397
  */
 
-export const dist_haversine = (location_a, location_b, sphere_radius = 1) => {
-	const degrees_to_radians = Math.PI / 180;
+export const dist_haversine = (location_a, location_b, sphere_radius) => {
+	sphere_radius ??= 1;
 
-	let lat_a = location_a.latitude * degrees_to_radians;
-	let lon_a = location_a.longitude * degrees_to_radians;
-	let lat_b = location_b.latitude * degrees_to_radians;
-	let lon_b = location_b.longitude * degrees_to_radians;
+	const lat_a = location_a.latitude  * TO_RADIANS;
+	const lon_a = location_a.longitude * TO_RADIANS;
+	const lat_b = location_b.latitude  * TO_RADIANS;
+	const lon_b = location_b.longitude * TO_RADIANS;
 
-	let dist_lat = lat_b - lat_a;
-	let dist_lon = lon_b - lon_a;
+	const dist_lat = lat_b - lat_a;
+	const dist_lon = lon_b - lon_a;
 
-	let a = Math.pow(Math.sin(dist_lat / 2), 2) + Math.cos(lat_a) * Math.cos(lat_b) * Math.pow(Math.sin(dist_lon / 2), 2);
-	let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
+	const a = Math.pow(Math.sin(dist_lat / 2), 2) + Math.cos(lat_a) * Math.cos(lat_b) * Math.pow(Math.sin(dist_lon / 2), 2);
+	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
 	
 	return c * sphere_radius;
 };
---
+++ dist_haversine.test.js
@@ -0,0 +1,26 @@
+import { test } from "./test.js";
+import { compare_floats } from "./compare_floats.js";
+import { dist_haversine } from "./dist_haversine.js";
+
+test("null island", () => {
+	return dist_haversine({ latitude: 0, longitude: 0 }, { latitude: 0, longitude: 0 }) === 0;
+});
+
+test("north pole to south pole", () => {
+	return compare_floats(dist_haversine({ latitude: 90, longitude: 0 }, { latitude: -90, longitude: 0 }), Math.PI);
+});
+
+test("if Australia was on the equator", () => {
+	return (() => {
+		const I = [];
+
+		for (let i = 0; i < 180; i++) {
+			I.push(i);
+		}
+
+		return I;
+	})().every((not_australia) => {
+		const australia = not_australia + 180;
+		return compare_floats(dist_haversine({ latitude: 0, longitude: australia }, { latitude: 0, longitude: not_australia }), Math.PI);
+	});
+});
