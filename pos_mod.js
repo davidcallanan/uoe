f14934e712eb255178e71de6412dce2e9343b7e6
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-01-03 19:50:51 +0000

    add pos_mod

---
+++ pos_mod.js
@@ -0,0 +1,10 @@
+/**
+ * @stability 4 - locked
+ * 
+ * Fixes modular arithmetic in JavaScript to always return a positive number.
+ */
+export const pos_mod = (a, b) => {
+	return ((a % b) + b) % b;
+};
+
+export const posMod = pos_mod;
