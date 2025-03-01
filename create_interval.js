38fceabf6dd43c619a6f81f62b22576cce19b2b2
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-03-01 17:22:53 +0000

    add create_interval

---
+++ create_interval.js
@@ -0,0 +1,9 @@
+export const create_interval = (period_ms, callback) => {
+	const handle = setInterval(callback, period_ms);
+	
+	return {
+		destroy: () => {
+			clearInterval(handle);
+		},
+	};
+};
