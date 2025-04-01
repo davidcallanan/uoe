2fa2a7b526581f131ef755eb96b28cee60253b40
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-04-01 16:41:51 +0100

    update collect_async

--- collect_async.js
+++ collect_async.js
@@ -1,10 +1,12 @@
 /**
+ * @stability 2 - provisional
+ * 
  * Collects all items from an async iterable into an array.
  */
 export const collect_async = async (async_iterable) => {
 	const result = [];
 
-	for await (const item of async_iterable) {
+	for await (const item of await async_iterable) {
 		result.push(item);
 	}
 	
