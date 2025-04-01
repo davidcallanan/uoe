d883aa51735d37390ec52890ccde4b34884ce9f1
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-04-01 23:20:40 +0100

    add mutate_variables

---
+++ mutate_variables.js
@@ -0,0 +1,24 @@
+import { collect_async } from "./collect_async.js";
+
+/**
+ * @stability 1 - experimental
+ */
+export const mutate_variables = async (coordinator, variables, callback) => {
+	const scopes = await collect_async(variables.map((variable) => variable.get_scope()));
+	const solo_variables = variables.map((variable) => variable.solo());
+	
+	await coordinator.run_transaction(scopes, async () => {
+		try {
+			await callback(solo_variables);
+		} catch (e) {
+			console.error(e);
+			// TODO: determine appropriate interface for error propagation.
+			return false;
+		}
+		
+		await Promise.all(variables.map((variable, i) => variable.man$force_swap(solo_variables[i])));
+		return true;
+	});
+};
+
+export const mutateVariables = mutate_variables;
