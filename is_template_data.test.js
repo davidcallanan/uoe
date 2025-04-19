a333399e629ca22773ed9617c0f313eb534f7df2
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-04-19 21:14:12 +0100

    add is_template_data

---
+++ is_template_data.test.js
@@ -0,0 +1,19 @@
+import { test } from "./test.js";
+import { is_template_data } from "./is_template_data.js";
+
+test("template data", () => {
+	const foo = (data) => is_template_data(data);
+	return foo`bar` === true;
+});
+
+test("non-template data", () => {
+	const foo = (data) => is_template_data(data);
+	return foo(`bar`) === false;
+});
+
+test("syntactic beauty", () => {
+	return (true
+		&& is_template_data`foo` === true
+		&& is_template_data(`bar`) === false
+	);
+});
