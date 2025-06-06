3e9e24d695f8e7aafe9703af1605ddc9a45277e4
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-06-06 15:11:10 +0100

    add unpacker_map

---
+++ unpacker_map.js
@@ -0,0 +1,174 @@
+import { is_enum } from "./is_enum.js";
+import { is_map } from "./is_map.js";
+import { map } from "./map.js";
+
+/**
+ * @stability 1 - experimental
+ * 
+ * Takes in an unpacker function and produces a map that destructures the various scenarios that the input might take on (whether the input be a uoe-map, a uoe-enum, or undefined).
+ * 
+ * Recall that "undefined" is the leaf case, specifying the desire to obtain the final return value of the map.
+ * 
+ * Otherwise, the input may be an enum or a map. Be careful not to inadvertently pass in a map that resolves to an enum leaf, when it is desired that the enum be passed in instead. Take care to await this if necessary.
+ * 
+ * TODO: should notion of uoe-api be incorporated directly into this notation?
+ * 
+ * @example
+ * 
+ * const my_map = unpacker_map(($) => {
+ *   $.symbol_name(($) => {
+ *     // ...
+ *   });
+ * 
+ * 	 $.symbol_name.$ret_lazy(() => "example");
+ *   $.symbol_name.$ret("example"); // shorthand
+ *   $.symbol_name.$call((map) => {});
+ *   $.symbol_name.$fall((inp) => {});
+ *   $.$ret(() => "example")
+ *   $.$("example"); // shorthand
+ *   $.$call((map) => {});
+ *   $.$fall((inp) => {});
+ * });
+ * 
+ * const person = unpacker_map(($) => {
+ *   $.name.$ret("John Doe");
+ *   $.age.$ret(27);
+ *   $.friends.$ret(["mary", "jane", "joseph"]);
+ *   $.address(($) => {
+ *     $.street.$ret("123 Main St");
+ *     $.city.$ret("Monopoly Town");
+ *     $.state.$ret("CA");
+ *   });
+ * });
+ */
+export const unpacker_map = (unpacker) => {
+	let self_call = undefined;
+	let self_fall = undefined;
+	let self_ret = undefined;
+	
+	const symbols = new Map();
+	
+	const builder = new Proxy({}, {
+		get: (target, prop) => {
+			if (prop === "$call") {
+				return (callback) => {
+					self_call = callback;	
+				};
+			}
+			
+			if (prop === "$fall") {
+				return (fallback) => {
+					self_fall = fallback;
+				};
+			}
+			
+			if (prop === "$ret") {
+				return (value) => {
+					self_ret = () => value;
+				};
+			}
+			
+			if (prop === "$ret_lazy") {
+				return (lazy_value) => {
+					self_ret = lazy_value;
+				};
+			}
+			
+			if (/^[a-zA-Z0-9_]+$/.test(prop)) {
+				return new Proxy((unpacker) => {
+					symbols.set(prop, symbols.get(prop) ?? {});
+					symbols.get(prop).fall = unpacker_map(unpacker);
+				}, {
+					get: (target, nested_prop) => {
+						if (nested_prop === "$call") {
+							return (callback) => {
+								symbols.set(prop, symbols.get(prop) ?? {});
+								symbols.get(prop).call = callback;
+							};
+						}
+						
+						if (nested_prop === "$fall") {
+							return (fallback) => {
+								symbols.set(prop, symbols.get(prop) ?? {});
+								symbols.get(prop).fall = fallback;
+							};
+						}
+						
+						if (nested_prop === "$ret") {
+							return (value) => {
+								symbols.set(prop, symbols.get(prop) ?? {});
+								symbols.get(prop).ret = () => value;
+							};
+						}
+						
+						if (nested_prop === "$ret_lazy") {
+							return (lazy_value) => {
+								symbols.set(prop, symbols.get(prop) ?? {});
+								symbols.get(prop).ret = lazy_value;
+							};
+						}
+						
+						return target[nested_prop];
+					},
+				});
+			}
+			
+			return target[prop];
+		},
+	});
+	
+	unpacker(builder);
+	
+	const symbol_maps = new Map();
+	
+	for (const [sym, entry] of symbols) {
+		symbol_maps.set(sym, unpacker_map(($) => {
+			if (entry.ret !== undefined) {
+				$.$ret_lazy(entry.ret);
+			}
+			
+			if (entry.call !== undefined) {
+				$.$call(entry.call);
+			}
+			
+			if (entry.fall !== undefined) {
+				$.$fall(entry.fall);
+			}
+		}));
+	}
+	
+	return map(async (input) => {
+		if (true
+			&& !is_map(input)
+			&& !is_enum(input)
+			&& input !== undefined
+		) {
+			throw user_payload_error("Expected uoe-map, uoe-enum or `undefined` as input to map. Consider using `obtain_map` or awaiting an enum leaf if appropriate.");
+		}
+		
+		if (true
+			&& self_ret !== undefined
+			&& input === undefined
+		) {
+			return await self_ret();
+		}
+		
+		if (true
+			&& self_call !== undefined
+			&& is_map(input)
+		) {
+			return await self_call(input);
+		}
+		
+		if (true
+			&& is_enum(input)
+			&& symbol_maps.get(input.sym) !== undefined
+		) {
+			return await symbol_maps.get(input.sym)(input.data);
+		}
+		
+		if (self_fall !== undefined) {
+			return await self_fall(input);
+		}
+	});
+};
---
+++ unpacker_map.test.js
@@ -0,0 +1,74 @@
+import { test } from "./test.js";
+import { unpacker_map } from "./unpacker_map.js";
+import { unsuspended_promise } from "./unsuspended_promise.js";
+
+await test("basic test", async () => {
+	const m = unpacker_map(($) => {
+		$.fall((input) => {
+			if (input === undefined) {
+				return "hello";
+			}
+			
+			return "world";
+		});
+	});
+
+	return await m() === "hello" && await m.foo() === "world";
+});
+
+await test("simple leaf values", async () => {
+	const person = unpacker_map(($) => {
+		$.name.$ret("John Doe");
+		$.age.$ret(27);
+		$.friends.$ret(["mary", "jane", "joseph"]);
+	});
+	
+	return (true
+		&& await person.name() === "John Doe"
+		&& await person.age() === 27
+		&& (friends => ["mary", "jane", "joseph"].every(entry => friends.includes(entry)))(await person.friends())
+	);
+});
+
+await test("nested leaf values", async () => {
+	const person = unpacker_map(($) => {
+		$.name.$ret("John Doe");
+		$.age.$ret(27);
+		$.friends.$ret(["mary", "jane", "joseph"]);
+		$.address(($) => {
+			$.street.$ret("123 Main St");
+			$.city.$ret("Monopoly Town");
+			$.state.$ret("CA");
+		});
+	});
+	
+	return (true
+		&& await person.name() === "John Doe"
+		&& await person.age() === 27
+		&& (friends => ["mary", "jane", "joseph"].every(entry => friends.includes(entry)))(await person.friends())
+		&& await person.address.street() === "123 Main St"
+		&& await person.address.city() === "Monopoly Town"
+		&& await person.address.state() === "CA"
+	);
+});
+
+await test("map calls", async () => {
+	const m = unpacker_map(($) => {
+		$.greet.$call(async (input) => {
+			return `Hello, ${await input.name()}!`;
+		});
+		
+		$.converse.$call(async (input) => {
+			return `How are you today, ${await input.name()}?`;
+		});
+	});
+	
+	const person = unpacker_map(($) => {
+		$.name.$ret("Jim");
+	});
+	
+	return (true
+		&& await m.greet(person)() === "Hello, Jim!"
+		&& await m.converse(person)() === "How are you today, Jim?"
+	);
+});
