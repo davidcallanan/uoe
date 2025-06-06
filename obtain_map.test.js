8789cd8dac61f2ab17365420722519997e6018ee
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-06-06 17:08:21 +0100

    test obtain_map update

--- obtain_map.test.js
+++ obtain_map.test.js
@@ -3,7 +3,7 @@ import { map } from "./map.js";
 import { unsuspended_promise } from "./unsuspended_promise.js";
 import { obtain_map } from "./obtain_map.js";
 
-const mix = (map) => {
+const mix_unsuspension = (map) => {
 	const m1 = map;
 	const p1 = unsuspended_promise(m1);
 	const m2 = obtain_map(p1);
@@ -13,6 +13,25 @@ const mix = (map) => {
 	const m4 = obtain_map(p3);
 
 	return m4;
+};
+
+const mix_wrapping = (map) => {
+	const m1 = map;
+	const m2 = obtain_map((async () => m1)());
+	const m3 = obtain_map((async () => m2)());
+	const m4 = obtain_map((async () => m3)());
+	
+	return m4;
+};
+
+const mix = (map) => {
+	const m1 = map;
+	const m2 = mix_unsuspension(m1);
+	const m3 = mix_wrapping(m2);
+	const m4 = mix_unsuspension(m3);
+	const m5 = mix_wrapping(m4);
+	
+	return m5;
 };
 
 await test("basic test", async () => {
