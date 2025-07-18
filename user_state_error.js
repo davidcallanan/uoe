b9df435f4d72611e73934c2343c49772b2aded60
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-07-18 22:02:46 +0100

    deprecate user_state_error

--- user_state_error.js
+++ user_state_error.js
@@ -1,3 +1,7 @@
+/**
+ * @stability 0 - deprecated
+ * @deprecated Superseded by `error_user_state`. Scheduled for removal in July 2027.
+ */
 export const user_state_error = (message, cause) => ({
 	status: "error",
 	error_type: "user_error",
