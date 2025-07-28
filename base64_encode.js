c8b2e538a3ba34485f7e4d8a36076bb7a203f06b
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-07-28 22:08:01 +0100

    add base64_encode

---
+++ base64_encode.js
@@ -0,0 +1,19 @@
+import { error_user_payload } from "./error_user_payload.js";
+import { throw_error } from "./throw_error.js";
+
+/**
+ * No-one should ever have the write a `base64_encode` function again.
+ * 
+ * Aside - Why is there still no better web standard than working with Latin-1 strings?
+ */
+export const base64_encode = (input) => {
+	if (!(input instanceof Uint8Array)) {
+		throw_error(error_user_payload("input must be uint8array"));
+	}
+
+	// Convert input into Latin-1 UTF-16, with the high byte zeroed.
+	// This safely encodes arbitrary binary data into the format expected by `btoa`.
+	const latin_string = Array.prototype.map.call(input, (byte) => String.fromCharCode(byte)).join("");
+
+	return btoa(latin_string);
+};
