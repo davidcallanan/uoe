555d4dd0de49ab66a53d3c1adce371ef21f26259
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-07-28 20:37:50 +0100

    add compare_bytes

---
+++ compare_bytes.js
@@ -0,0 +1,30 @@
+import { error_user_payload } from "./error_user_payload.js";
+import { throw_error } from "./throw_error.js";
+
+/**
+ * @stability 2 - provisional
+ * 
+ * Aside - why does JavaScript not have an inbuilt function for this?
+ */
+export const compare_bytes = (a, b) => {
+	if (false
+		|| !a instanceof Uint8Array
+		|| !b instanceof Uint8Array
+	) {
+		throw_error(error_user_payload("inputs must be uint8array"));
+	}
+
+	if (a.length !== b.length) {
+		return false;
+	}
+
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] !== b[i]) {
+			return false;
+		}
+	}
+
+	return true;
+};
+
+export const compareBytes = compare_bytes;
