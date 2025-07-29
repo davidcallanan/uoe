12037c4709786c806028445750953951d3468bbb
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-07-29 17:36:48 +0100

    add web/request_upload

---
+++ web/request_upload.js
@@ -0,0 +1,29 @@
+import { create_promise } from "../create_promise.js";
+
+export const receive_upload = (document, options) => {
+	options ??= {};
+	options.allow_multiple ??= false;
+
+	const input = document.createElement("input");
+	input.style.display = "none";
+	input.setAttribute("type", "file");
+
+	if (options.allow_multiple) {
+		input.setAttribute("multiple", "multiple");
+	}
+
+	document.body.appendChild(input);
+	input.click();
+
+	const [promise, res] = create_promise();
+
+	input.addEventListener("change", (event) => {
+		res(Array.from(event.target.files));
+	});
+
+	input.addEventListener("cancel", () => {
+		res([]);
+	});
+
+	return promise;
+};
