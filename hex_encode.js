4dcecb6d83991f656c9159733fbdbf112a52b557
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-07-29 17:37:30 +0100

    add hex_encode

---
+++ hex_encode.js
@@ -0,0 +1,15 @@
+import { error_user_payload } from "./error_user_payload.js";
+import { throw_error } from "./throw_error.js";
+
+/**
+ * @stability 2 - provisional
+ */
+export const hex_encode = (bytes) => {
+	if (!(bytes instanceof Uint8Array)) {
+		throw_error(error_user_payload("bytes must be uint8array"));
+	}
+
+	return Array.from(bytes).map((byte) => byte.toString(16).padStart(2, "0")).join("");
+};
+
+export const hexEncode = hex_encode;
