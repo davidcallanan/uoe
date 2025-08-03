a4c5fb298ce38f4cc976f5d398dd0f5f5af30f0c
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-08-03 12:59:13 +0100

    add node/create_default_file

---
+++ node/create_default_file.js
@@ -0,0 +1,18 @@
+/**
+ * @stability 2 - provisional
+ * 
+ * Creates a file with the desired data if it does not already exist.
+ */
+export const create_default_file = async (dependencies, path, data) => {
+	try {
+		await dependencies.fs.writeFile(path, data, { flag: "wx" });
+	} catch (e) {
+		if (e.code === "EEXIST") {
+			return;
+		}
+
+		throw e;
+	}
+};
+
+export const createDefaultFile = create_default_file;
