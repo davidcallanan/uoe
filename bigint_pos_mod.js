a79e4d510691a9116d3b631b03e2f67f9c7728ba
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-08-31 12:54:42 +0100

    add bigint_pos_mod

---
+++ bigint_pos_mod.js
@@ -0,0 +1,10 @@
+/**
+ * @stability 3 - stable
+ * 
+ * Fixes modular arithmetic in JavaScript to always return a positive number.
+ */
+export const bigint_pos_mod = (a, b) => {
+	return ((a % b) + b) % b;
+};
+
+export const bigintPosMod = bigint_pos_mod;
