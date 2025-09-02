57132e4d4aed458e22c45148d01893c738bff389
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2025-09-02 17:19:38 +0100

    add shallow

---
+++ shallow.js
@@ -0,0 +1,8 @@
+/**
+ * @stability 3 - stable
+ * 
+ * Shallow-clones an object while simultaneously defaulting it to an empty object.
+ */
+export const shallow = (object) => {
+	return { ...(object ?? {}) };
+};
